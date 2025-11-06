8e9a95c221d8b2681ee22b73518a3e82159c7d66
lLuizSales <luiz.edu.car33@gmail.com>
2025-11-05 21:07:06 -0300

    insecaoOrdenada

--- aplicacao/fila-transacoes.c
+++ aplicacao/fila-transacoes.c
@@ -16,7 +16,42 @@ Lista *criarLista(){
 
 int insercaoOrdenada(Lista *li, Transacao nova_tran){
 
-    
+    Elemento *novo = (Elemento *)malloc(sizeof(Elemento));
+
+    novo->dados = nova_tran;
+    novo->prox = NULL;
+
+    if(li == NULL) return 0;
+
+    if((*li) == NULL){
+
+        *li = novo;
+
+        return 1;
+    }
+
+    Elemento *atual = *li;
+
+    while(novo->dados.prioridade < atual->dados.prioridade){
+
+        atual = atual->prox;
+
+    }
+
+    if(atual->prox == NULL){
+
+        atual->prox = novo;
+
+        return 1;
+
+    } else if (atual->prox != NULL){
+
+        novo->prox = atual->prox;
+        atual->prox = novo;
+        
+        return 1;
+
+    }
 
 }
 
@@ -24,6 +59,6 @@ int exclusaoOrdenada(Lista *li){
 
 }
 
-int impressirFila(Lista *li){
+int imprimirFila(Lista *li){
 
 }
